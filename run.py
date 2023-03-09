c2f874c0ecc44280ecdbbf377914580b5c788690
AmarDange <amardange218@gmail.com>
2023-03-09 18:10:06 +0000

    Add run.py file

--- run.py
+++ run.py
@@ -1,3 +1,89 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+print("Welcome to my computer quiz")
+
+playing = input("Do you wnat to play?")
+
+if playing !="yes":
+    quit()
+
+print("Okay! Let's play! :)")
+score = 0
+
+answer = input("Is the coding langyage Python, named after a snake? ")
+if answer == "no":
+    print('correct!')
+    score += 1
+else:
+    print('Incorrect!')
+
+answer = input("What does CPU stand for?")
+if answer =="central processing unit":
+    print('Correct')
+    score += 1
+else:
+    print('Incorrect')
+
+answer = input("What does RAM stand for?")
+if answer =="random access memory":
+    print('Correct')
+    score += 1
+else:
+    print('Incorrect')
+
+answer = input("What does HTML stand for?")
+if answer =="hypertext markup language":
+    print('Correct')
+    score += 1
+else:
+    print('Incorrect')
+
+answer = input("What does PDF stand for?")
+if answer =="portable document format":
+    print('Correct')
+    score += 1
+else:
+    print('Incorrect')
+
+answer = input("What does PNG in computer image format stand for?")
+if answer =="portable network graphics":
+    print('Correct')
+    score += 1
+else:
+    print('Incorrect')
+
+
+answer = input("What does GB in computer stand for?")
+if answer =="giga bytes":
+    print('Correct')
+    score += 1
+else:
+    print('Incorrect')
+
+
+answer = input("What does CD in computer stand for?")
+if answer =="compact disk":
+    print('Correct')
+    score += 1
+else:
+    print('Incorrect')
+
+
+answer = input("What does DVD stand for?")
+if answer =="digital video disk":
+    print('Correct')
+    score += 1
+else:
+    print('Incorrect')
+
+
+answer = input("What does KB stand for?")
+if answer =="kilo bytes":
+    print('Correct')
+    score += 1
+else:
+    print('Incorrect')
+
+
+print("You got " + str(score) + " questoins correct!")
