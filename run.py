a45f500a2adb1aa42bcf42d2b7e26882d5f76dcf
AmarDange <amardange218@gmail.com>
2023-03-09 20:49:00 +0000

    add percentage to get answer

--- run.py
+++ run.py
@@ -3,7 +3,7 @@
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
 print("Welcome to my computer quiz")
 
-playing = input("Do you wnat to play?")
+playing = input("Do you want to play?")
 
 if playing !="yes":
     quit()
@@ -13,77 +13,78 @@ score = 0
 
 answer = input("Is the coding langyage Python, named after a snake? ")
 if answer == "no":
-    print('correct!')
+    print('correct!!')
     score += 1
 else:
-    print('Incorrect!')
+    print('Incorrect!!!')
 
 answer = input("What does CPU stand for?")
 if answer =="central processing unit":
-    print('Correct')
+    print('Correct!!')
     score += 1
 else:
-    print('Incorrect')
+    print('Incorrect!!!')
 
 answer = input("What does RAM stand for?")
 if answer =="random access memory":
-    print('Correct')
+    print('Correct!!')
     score += 1
 else:
-    print('Incorrect')
+    print('Incorrect!!!')
 
 answer = input("What does HTML stand for?")
 if answer =="hypertext markup language":
-    print('Correct')
+    print('Correct!!')
     score += 1
 else:
-    print('Incorrect')
+    print('Incorrect!!!')
 
 answer = input("What does PDF stand for?")
 if answer =="portable document format":
-    print('Correct')
+    print('Correct!!')
     score += 1
 else:
-    print('Incorrect')
+    print('Incorrect!!!')
 
 answer = input("What does PNG in computer image format stand for?")
 if answer =="portable network graphics":
-    print('Correct')
+    print('Correct!!')
     score += 1
 else:
-    print('Incorrect')
+    print('Incorrect!!!')
 
 
 answer = input("What does GB in computer stand for?")
 if answer =="giga bytes":
-    print('Correct')
+    print('Correct!!')
     score += 1
 else:
-    print('Incorrect')
+    print('Incorrect!!!')
 
 
 answer = input("What does CD in computer stand for?")
 if answer =="compact disk":
-    print('Correct')
+    print('Correct!!')
     score += 1
 else:
-    print('Incorrect')
+    print('Incorrect!!!')
 
 
 answer = input("What does DVD stand for?")
 if answer =="digital video disk":
-    print('Correct')
+    print('Correct!!')
     score += 1
 else:
-    print('Incorrect')
+    print('Incorrect!!!')
 
 
 answer = input("What does KB stand for?")
 if answer =="kilo bytes":
-    print('Correct')
+    print('Correct!!')
     score += 1
 else:
-    print('Incorrect')
+    print('Incorrect!!!')
 
 
 print("You got " + str(score) + " questoins correct!")
+print("You got" + str((score/10) *100) + "%. ")
